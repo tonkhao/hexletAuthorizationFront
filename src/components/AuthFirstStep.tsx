992d23549e1e5b360aa3cf90bd1b39738381580b
tonkhao <33198781+tonkhao@users.noreply.github.com>
2025-12-16 18:51:33 +0300

    html structure in progress

--- src/components/AuthFirstStep.tsx
+++ src/components/AuthFirstStep.tsx
@@ -1,15 +1,20 @@
 export function AuthFirstStep() {
+  const digInputsRender = () => {
+    for (let index = 0; index < 6; index++) {
+      return <input className="textInput" type="text" />
+    }
+  }
+  
   return (
     <>
-      <div className="companyLogo">LOGO</div>
-      <div className="signInText">
-        <p>Sign in to your account to continue</p>
+      <div className="companyLogo">
+        LOGO
+        <p className="signInText">Sign in to your account to continue</p>
       </div>
-      <div className="inputsWrapper">
-        <input className="textInput" type="text" />
-        <input className="textInput" type="text" />
+      <div className="formWrapper">
+        { digInputsRender }
       </div>
-      <button onClick={() => console.log("clock")}>Log in</button>
+      <input className="buttonInput" type="button" value="Log in" onClick={() => console.log("clock")} />
     </>
   );
 }
--- src/components/AuthSecondStep.tsx
+++ src/components/AuthSecondStep.tsx
@@ -1,3 +1,20 @@
 export function AuthSecondStep() {
-    return "second"
+  return (
+    <>
+      <div className="companyLogo">
+        LOGO
+        <p className="TwoFactorAuthText">Sign in to your account to continue</p>
+         <p className="TwoFactorAuthTextDesc">Sign in to your account to continue</p>
+      </div>
+      <div className="formWrapper">
+        
+      </div>
+      <input
+        className="buttonInput"
+        type="button"
+        value="Log in"
+        onClick={() => console.log("clock")}
+      />
+    </>
+  );
 }
