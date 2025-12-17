ba68791f78f9f325f7fb0ec020a8654af637441f
tonkhao <33198781+tonkhao@users.noreply.github.com>
2025-12-17 13:25:56 +0300

    fist step basic css style done

--- src/components/AuthFirstStep.tsx
+++ src/components/AuthFirstStep.tsx
@@ -1,4 +1,6 @@
+import { LockedIcon } from "../assets/LockedIcon";
 import { LogoIcon } from "../assets/LogoIcon";
+import { UserIcon } from "../assets/UserIcon";
 
 export function AuthFirstStep() {
   const checkFirstStepAuth = () => {
@@ -11,12 +13,28 @@ export function AuthFirstStep() {
           <LogoIcon />
         </div>
         <div className="descriptionContainer">
-          <p className="signInText">Sign in to your account to<br />continue</p>
+          <p className="signInText">
+            Sign in to your account to
+            <br />
+            continue
+          </p>
         </div>
       </div>
-      <div className="formWrapper"></div>
-      <input className="textInput" type="text" />
-      <input className="textInput" type="password" />
+      <div className="inputsOutterWrapper">
+        <div className="inputContainer">
+          <div className="iconContainer">
+            <UserIcon />
+          </div>
+          <input className="textInput" type="text" />
+        </div>
+        <div className="inputContainer">
+          <div className="iconContainer">
+            <LockedIcon />
+          </div>
+          <input className="textInput" type="password" />
+        </div>
+      </div>
+
       <input
         className="buttonInput"
         type="button"
